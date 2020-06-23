3d21901b2fd3a59982aa091bf552440b63ab48d8
Jonathan <jonretchless@gmail.com>
2020-06-23 13:17:14 -0700

    added localhost

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://whispering-earth-32320.herokuapp.com'
+  API_ENDPOINT:
+    // 'https://whispering-earth-32320.herokuapp.com'
+    'http://localhost:8000'
 }
